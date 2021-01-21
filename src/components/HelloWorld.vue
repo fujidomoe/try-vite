df4ca903872b375aaea89b71c471f8f2f736eb59
sakura1116vg <daisuke_sasaki@voyagegroup.com>
2021-01-21 17:40:42 +0900

    axios, get https://jsonplaceholder.typicode.com/posts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,33 +2,59 @@
   <section class="alert alert-primary">
     <h1>{{data.title}}</h1>
     <p>{{data.message}}</p>
-    <textarea v-model="data.mydata" rows="5"
-      class="form-control"></textarea>
+    <div class="form-group">
+      <input type="number" class="form-control"
+        v-model="data.id" />
+      <button class="btn btn-primary m-2"
+        @click="doClick">Click</button>
+    </div>
+    <table class="table table-light table-striped">
+      <tbody class="text-left">
+      <tr>
+        <th style="width:200px;">User ID</th>
+        <td>{{data.json_data ? data.json_data.userId : '-'}}</td>
+      </tr>
+      <tr>
+        <th>ID</th>
+        <td>{{data.json_data ? data.json_data.id : '-'}}</td>
+      </tr>
+      <tr>
+        <th>Title</th>
+        <td>{{data.json_data ? data.json_data.title : '-'}}</td>
+      </tr>
+      <tr>
+        <th>Body</th>
+        <td>{{data.json_data ? data.json_data.body : '-'}}</td>
+      </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <script>
 import axios from 'axios'
-import { reactive, onMounted } from 'vue'
+import { reactive } from 'vue'
 
+let url = "https://jsonplaceholder.typicode.com/posts/"
       
 export default {
   setup(props) {
     const data = reactive({
       title:'Axios',
       message:'This is axios sample.',
-      mydata: '',
+      id:0,
+      json_data: null,
     })
-    const getData = () => {
-      const url = "/data.txt"
-      axios.get(url).then((result)=> {
-        data.mydata = result.data
+    const doClick = ()=> {
+      axios.get(url + data.id).then((result)=> {
+        data.json_data = result.data
+        data.message = 'SUCESS!'
+      }).catch((error) => {
+        data.message = 'ERROR!'
+        data.json_data = null
       })
     }
-    onMounted(()=> {
-      getData()
-    })
-    return { data, getData }
+    return { data, doClick }
   },
 }
 </script>
