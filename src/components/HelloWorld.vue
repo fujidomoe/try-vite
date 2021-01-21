a08b677226cd64cb33e4d3328b82f00b4d494d09
sakura1116vg <daisuke_sasaki@voyagegroup.com>
2021-01-21 11:10:24 +0900

    ...

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="alert alert-info">
-    <h2>{{ title }}</h2>
-    <p>{{ message }}</p>
-    <hr>
-    <div>
-      <input class="form-control" type="text" v-model="input">
-      <button class="btn btn-info mt-2" v-on:click="doAction">Click</button>
-    </div>
+  <div class="alert alert-primary">
+    <h1>{{ title }}</h1>
+    <p>{{ data.msg }}( {{ data.count }} )</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-  name: 'HelloWorld',
   props: {
     title: String,
   },
-  data() {
+  setup(props){
+    const data = ref({
+      msg: 'This is ref-value!',
+      count: 0
+    })
+    setInterval(()=> {
+      data.value.count++
+    }, 1000)
     return {
-      message: 'お名前は?',
-      input: 'no name',
-    }
-  },
-  methods: {
-    doAction() {
-      this.message = 'こんにちは、' + this.input + 'さん！'
-      this.$emit('result-event', this.input)
+      data
     }
   }
 }
